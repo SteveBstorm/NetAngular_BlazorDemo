a7c11e349bbc084bd13e86f438e238eb098e279b
stevebstorm <steve.lorent@gmail.Com>
2023-06-13 15:57:11 +0200

    AjoutSignalR + Conso API

--- BlazorDemo/Client/Pages/Exercices/Exo2/Exo2.razor.cs
+++ BlazorDemo/Client/Pages/Exercices/Exo2/Exo2.razor.cs
@@ -1,38 +1,54 @@
-﻿namespace BlazorDemo.Client.Pages.Exercices.Exo2
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorDemo.Client.Pages.Exercices.Exo2
 {
     public partial class Exo2
     {
         public List<Game> Games { get; set; }
-        public Game SelectedGame { get; set; }
+        public int SelectedGame { get; set; }
+
+        [Inject]
+        public HttpClient client { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Games = await client.GetFromJsonAsync<List<Game>>("game");
+        }
 
         public Exo2()
         {
             Games = new List<Game>();
-            Games.Add(new Game
-            {
-                Id = Games.Count + 1,
-                Title = "Diablo 4",
-                Genre = "Hack'n slash",
-                ReleaseYear = 2023,
-                Note = 2
-            });
+            //Games.Add(new Game
+            //{
+            //    Id = Games.Count + 1,
+            //    Title = "Diablo 4",
+            //    Genre = "Hack'n slash",
+            //    ReleaseYear = 2023,
+            //    Note = 2
+            //});
         }
 
-        public void AddGame(GameForm form)
+        //A modifier
+        public async Task AddGame(GameForm form)
         {
-            Games.Add(new Game
+            Game myGame = new Game
             {
-                Id = Games.Count + 1,
+                //Id = Games.Count + 1,
                 Title = form.Title,
                 Genre = form.Genre,
                 ReleaseYear = form.ReleaseYear,
                 Note = form.Note
-            });
+            };
+
+            Task t = client.PostAsJsonAsync<Game>("game", myGame);
+            //if(t.IsCompleted)
+            //    Games = await client.GetFromJsonAsync<List<Game>>("game");
         }
 
         public void SelectGame(int id)
         {
-            SelectedGame = Games.FirstOrDefault(x => x.Id == id);
+            SelectedGame = id;
         }
     }
 }
